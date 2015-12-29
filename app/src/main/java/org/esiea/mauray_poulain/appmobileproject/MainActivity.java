7ef9f3abad1750f9c9961f6bb3fe44916052c770
maxpoulain <poulain@et.esiea.fr>
2015-12-29 23:32:05 +0100

    changement actionbar + ajout GIF

--- app/src/main/java/org/esiea/mauray_poulain/appmobileproject/MainActivity.java
+++ app/src/main/java/org/esiea/mauray_poulain/appmobileproject/MainActivity.java
@@ -2,6 +2,8 @@ package org.esiea.mauray_poulain.appmobileproject;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,13 +11,20 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Timer;
+
+import pl.droidsonroids.gif.GifDrawable;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public DatePickerDialog dpd;
+
     private static final String TAG = MainActivity.class.getSimpleName();
 
 
@@ -23,29 +32,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-       /* final TextView tv_hw = (TextView) findViewById(R.id.tv_hello_world);
-        Button btn_hw = (Button) findViewById(R.id.btn_hello_world);
-        String now = DateUtils.formatDateTime(getApplicationContext(), (new Date()).getTime(), DateFormat.FULL);
-        //getString(R.string.hello);
-        tv_hw.setText(now);
-        final Calendar c = Calendar.getInstance();
-        int mYear = c.get(Calendar.YEAR);
-        int mMonth = c.get(Calendar.MONTH);
-        int mDay = c.get(Calendar.DAY_OF_MONTH);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
 
-        dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                tv_hw.setText(day + "-" + month + "-" + year);
-            }
-        }, mYear, mMonth, mDay);*/
+        Log.d(TAG, "LANGUAGE : " + Locale.getDefault().getDisplayLanguage());
 
+        if(Locale.getDefault().getDisplayLanguage().contains("English")){
+            toolbar.setTitle("Beer Palace");
+            Log.d(TAG, "ENGLISH");
+        }else if(Locale.getDefault().getDisplayLanguage().contains("fra")) {
+            toolbar.setTitle("Palais de la Bière");
+        }
 
         setSupportActionBar(toolbar);
 
-    }
+        try {
 
+
+
+        new Thread(new Runnable() {
+            GifDrawable gifFromResource = new GifDrawable(getResources(), R.drawable.giphy);
+            @Override
+            public void run() {
+                try{
+                    gifFromResource.start();
+                    synchronized(this){
+                        wait(3560);
+                    }
+                    gifFromResource.stop();
+                    IntentFonction();
+                    GetBierServices.startActionBiers(getApplicationContext());
+                    Log.d(TAG, "LANCEMENT OK");
+                }catch (InterruptedException e){
+
+                }
+            }
+        }).start();
+
+
+        }catch (IOException e){
+
+        }
+
+
+    }
 
 
     @Override
@@ -63,7 +93,17 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_settings) {
+            return true;
+        }
+        if (id == R.id.menu_search) {
+            Toast toast = Toast.makeText(this,"Cette fonctionnalité est disponible dans la version payante de l'application",Toast.LENGTH_SHORT);
+            toast.show();
+            return true;
+        }
+        if(id==R.id.menu_about){
+            Toast toast = Toast.makeText(this,"Cette application a été codée par Maxime Mauray & Maxime Poulain",Toast.LENGTH_SHORT);
+            toast.show();
             return true;
         }
 
